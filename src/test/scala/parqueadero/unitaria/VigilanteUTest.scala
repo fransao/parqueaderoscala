a677e2f17681a4cfa6dd64eee4b7cffed9651368
fransao <francisco-salazar8a@hotmail.com>
2018-07-20 10:17:20 -0500

    prueba scala

---
+++ src/test/scala/parqueadero/unitaria/VigilanteUTest.scala
@@ -0,0 +1,10 @@
+package parqueadero.unitaria
+
+import org.scalatest.FlatSpec
+
+class VigilanteUTest extends FlatSpec {
+
+  "An empty Set" should "have size 0" in {
+    assert(Set.empty.size == 0)
+  }
+}
